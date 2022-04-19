fb96a66f468af1b74bf759d782f473a92c565873
mohamedbntag <mohamedbntag@gmail.com>
2022-04-19 23:43:26 +0200

    created new files

---
+++ js/js.js
@@ -0,0 +1,40 @@
+/*eslint no-use-before-define: "error"*/
+$(document).ready(function () {
+    
+    $(".code").hide();
+
+   $(".btn1").click(function(){
+       $(".code1").slideToggle(2000);
+   })
+        
+    $(".btn2").click(function(){
+       $(".code2").slideToggle(2000);
+   })
+    
+     $(".btn3").click(function(){
+       $(".code3").slideToggle(2000);
+   })
+    
+    $(".btn4").click(function(){
+       $(".code4").slideToggle(2000);
+   })
+    
+    $(".btn5").click(function(){
+       $(".code5").slideToggle(2000);
+   })
+    
+    $(".btn6").click(function(){
+       $(".code6").slideToggle(2000);
+   })
+    
+    $(".btn7").click(function(){
+       $(".code7").slideToggle(2000);
+   })
+    
+    $(".btn8").click(function(){
+       $(".code8").slideToggle(2000);
+   })
+    
+    });
+    
+    
---
+++ js/learnJs.js
@@ -0,0 +1,7 @@
+let hi = `
+    <div class="head">
+        <h1>title</h1>
+        <p>this paragraph element </p>
+    </div>
+`;
+document.write(hi)
